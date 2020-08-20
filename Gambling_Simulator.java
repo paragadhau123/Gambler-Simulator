f76cdf5d04cb247c0e73ea1edbe028b99f1f343d
parag <adhauparag64@gmail.com>
2020-08-20 21:56:43 +0530

    added gambling limit for a day

--- Gambling_Simulator.java
+++ Gambling_Simulator.java
@@ -8,19 +8,22 @@ import java.util.Random;
           int lostStake = 100;
           int r1,r2;
           Random r = new Random();
-          while(day == 1){
+          while(winStake<150 || lostStake>50){
              int random = r.nextInt(stake);
              r1 = random%10;
              r2 = r1%2;
              if(r2 == 0){
                  lostStake = lostStake-1;
-                 System.out.println("*******Gambler Loose********");
                 }
              else{
                  winStake = winStake+1;
-                 System.out.println("*******Gambler Win*******");
                 }
-             day = day+1;
             }
-          }
-        }
+              if(winStake==150){
+                 System.out.println("Gambler win 50% of stake");
+               }
+             else{
+                 System.out.println("Gambler loose 50% of stake");
+                }
+             }
+         }
