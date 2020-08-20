c29fab5d3ca84fa403016aec2f5e7423c1961b57
parag <adhauparag64@gmail.com>
2020-08-20 22:40:16 +0530

    added functionality to print daily detail result for a month

--- Gambling_Simulator.java
+++ Gambling_Simulator.java
@@ -1,21 +1,27 @@
 import java.util.Random;
 
  public class Gambling_Simulator{
+
      public static void main(String [] args){
+
           int stake = 100;
           int day = 1;
-          int winAmount;
-          int lostAmount;
           int r1,r2;
           Random r = new Random();
-          while(day<=20){
+
+          while(day<=30){
+
            int winStake = 100;
            int lostStake = 100;
+           int winAmount;
+           int lostAmount;
 
           while(winStake<150 || lostStake>50){
+
              int random = r.nextInt(stake);
              r1 = random%10;
              r2 = r1%2;
+
              if(r2 == 0){
                  lostStake = lostStake-1;
                 }
@@ -23,10 +29,20 @@ import java.util.Random;
                  winStake = winStake+1;
                 }
             }
-               winAmount=winStake-stake;
-               lostAmount=stake-lostStake;
+
+               winAmount = winStake-stake;
+               lostAmount = stake-lostStake;
+
                System.out.println("Win amount of day"+day+" is :"+winAmount+" and Lost amount of day"+day+" is :"+lostAmount);
-               day=day+1;
+
+               if( winAmount > lostAmount ){
+                System.out.println("Day"+day+" win by "+(winAmount-lostAmount));
+                }
+             else{
+                System.out.println("Day"+day+" lost by "+(lostAmount-winAmount));
+                }
+
+                day=day+1;
               }
             }
          }
