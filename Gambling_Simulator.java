2c53d36a0a21b5fe0bb44e6a0563cc12a61232b5
parag <adhauparag64@gmail.com>
2020-08-21 21:16:34 +0530

    Modified the code of finding luckiest and unluckiest day of month

--- Gambling_Simulator.java
+++ Gambling_Simulator.java
@@ -1,18 +1,21 @@
+import java.util.Arrays;
 import java.util.Random;
 
  public class Gambling_Simulator{
 
      public static void get30DaysReport(){
-           int winAmount=0;
-           int lostAmount=0;
+           int winAmount = 0;
+           int lostAmount = 0;
        do{
           int stake = 100;
           int day = 1;
           int r1,r2;
-          int maxAmountWin=Integer.MIN_VALUE;
-          int maxAmountLost=Integer.MAX_VALUE;
-          int luckiestDay=0;
-          int unluckiestDay=0;
+          //int maxAmountWin=Integer.MIN_VALUE;
+          //int maxAmountLost=Integer.MAX_VALUE;
+          //int luckiestDay=0;
+          //int unluckiestDay=0;
+          int arr[] = new int[30];
+          int dailyWin = 0;
           //int totalWin = 0;
           //int totalLost = 0;
           Random r = new Random();
@@ -27,14 +30,18 @@ import java.util.Random;
           while( winStake<150 || lostStake>50 ){
 
              int random = r.nextInt(stake);
-             r1 = random%10;
-             r2 = r1%2;
+             r1 = random % 10;
+             r2 = r1 % 2;
 
              if(r2 == 0){
-                 lostStake = lostStake-1;
+                 lostStake = lostStake - 1;
+                 dailyWin = dailyWin - 1;
+                 arr[day-1] = dailyWin;
                 }
              else{
                  winStake = winStake+1;
+                 dailyWin = dailyWin+1;
+                 arr[day-1] = dailyWin;
                 }
             }
 
@@ -51,21 +58,40 @@ import java.util.Random;
                   System.out.println("Day"+day+" lost by "+(lostAmount-winAmount));
                  }
 
-                 if( winAmount > maxAmountWin ){
-                     maxAmountWin = winAmount;
-                     luckiestDay = day;
-                   }
-                else if( lostAmount < maxAmountLost ){
-                     maxAmountLost = lostAmount;
-                     unluckiestDay = day;
-                   }
-
                 day = day+1;
               }
-               System.out.println("Luckiest Day of month is "+luckiestDay+" and won amount "+maxAmountWin);
-               System.out.println("Unluckiest Day of month is "+unluckiestDay+" and won amount "+maxAmountLost);
-              }while(winAmount>lostAmount);
+               //System.out.println("Luckiest Day of month is "+luckiestDay+" and won amount "+maxAmountWin);
+               //System.out.println("Unluckiest Day of month is "+unluckiestDay+" and won amount "+maxAmountLost);
+               GetLuckyAndUnluckytDay(arr);
+              }while( winAmount > lostAmount );
            }
+               public static void GetLuckyAndUnluckytDay(int arr[]){
+
+                int luckyDay = 0;
+                int unluckyDay = 0;
+                int high = arr[0];
+                int low = arr[0];
+                for (int i = 1; i < arr.length; i++)
+                {
+                        if( high < arr[i] )
+                         {
+                                high = arr[i];
+                                luckyDay = i+1;
+                        }
+                        if( low > arr[i] )
+                        {
+                                low = arr[i];
+                                unluckyDay = i+1;
+                        }
+
+
+                }
+                System.out.println("Luckiest day is day : "+luckyDay+" and He won : "+high);
+                System.out.println("Unluckiest day is day : "+unluckyDay+" and He lost : "+low);
+
+        }
+
+
 
              public static void main(String [] args){
                  get30DaysReport();
